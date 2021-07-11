c9e1e2094062669c25ca79aa26c7fa9f0490f0d2
myarlagadda <mouni.yarlagadda@icloud.com>
2021-07-11 13:52:05 -0400

    Test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React!
+          Day 1 of Learning React!
         </a>
       </header>
     </div>
