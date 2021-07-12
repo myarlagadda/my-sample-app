364f5215ef2a7f76f61e06220052f4d6d994f692
myarlagadda <mouni.yarlagadda@icloud.com>
2021-07-12 07:34:57 -0400

    Components and Props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from "react-dom";
+import ExpenseDate from './ExpenseDate';
 
 import ExpenseItemStyles from './ExpenseItem.css';
 
@@ -7,7 +8,7 @@ function ExpenseItem(props) {
 
     return (
         <div className="expense-item">
-            <div>{props.date.toISOString()}</div>
+            <ExpenseDate date={props.date}/>
             <div className="expense-item__description"><h2>{props.title}</h2>
                 <div className="expense-item__price">{props.amount}</div>
             </div>
