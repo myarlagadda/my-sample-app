ed764c72cbef76f7b110f5b8400ee838646ea7b2
myarlagadda <mouni.yarlagadda@icloud.com>
2021-07-11 21:52:47 -0400

    Added new changes for test

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import ExpenseItemStyles from './ExpenseItem.css';
 
-function ExpenseItem()
-{
-    return <h2> Expense Item!!</h2>
+function ExpenseItem() {
+    return (
+        <div className="expense-item">
+            <div>July 11 2021</div>
+            <div className="expense-item__description"><h2>Car Insurance</h2>
+                <div className="expense-item__price">28.32</div>
+            </div>
+        </div>
+    );
 }
-
 export default ExpenseItem;
