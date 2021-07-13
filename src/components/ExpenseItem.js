a5df8728e619a802b98f71aa74be181c793f1338
myarlagadda <mouni.yarlagadda@icloud.com>
2021-07-13 06:51:55 -0400

    useState on Button Click

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from "react-dom";
 import ExpenseDate from './ExpenseDate';
 import Card from './Card';
@@ -6,13 +6,21 @@ import Card from './Card';
 import ExpenseItemStyles from './ExpenseItem.css';
 
 function ExpenseItem(props) {
+    const [title, setTitle] = useState(props.title);
+    const [amount, setAmount] = useState(props.amount);
+    console.log(title);
+    const ClickHandler = () => {
+        setTitle('Updated!');
+        setAmount('111.11');
+    }
 
     return (
         <Card className="expense-item">
-            <ExpenseDate date={props.date}/>
-            <div className="expense-item__description"><h2>{props.title}</h2>
-                <div className="expense-item__price">{props.amount}</div>
+            <ExpenseDate date={props.date} />
+            <div className="expense-item__description"><h2>{title}</h2>
+                <div className="expense-item__price">{amount}</div>
             </div>
+            <button onClick={ClickHandler}>Click Here!</button>
         </Card>
     );
 }
